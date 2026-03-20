c6bab365c1d98cf4fdf75fe380045bfb7af65761
gunjangera <geragunjan02@gmail.com>
2026-03-20 19:36:35 +0530

    add js

---
+++ logic.js
@@ -0,0 +1,24 @@
+let aboutBtn = document.querySelector("#about-btn");
+aboutBtn.addEventListener("click", () => {
+    document.querySelector(".about").scrollIntoView({ behavior: "smooth" });
+});
+
+let skillsBtn = document.querySelector("#skills-btn");
+skillsBtn.addEventListener("click", () => {
+    document.querySelector(".skill-section").scrollIntoView({ behavior: "smooth" });
+});
+
+let projectsBtn = document.querySelector("#projects-btn");
+projectsBtn.addEventListener("click", () => {
+    document.querySelector(".project").scrollIntoView({ behavior: "smooth" });
+});
+
+let educationBtn = document.querySelector("#education-btn");
+educationBtn.addEventListener("click", () => {
+    document.querySelector(".education").scrollIntoView({ behavior: "smooth" });
+});
+
+let contactBtn = document.querySelector("#contact-btn");
+contactBtn.addEventListener("click", () => {
+    document.querySelector(".contact").scrollIntoView({ behavior: "smooth" });
+});
